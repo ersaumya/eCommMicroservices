5ac9fe743c5fd4d64460f4f30394ad72c2b98982
ersaumya <ersaumyarout@gmail.com>
2020-08-03 02:39:46 +0530

    added mongodb settings

--- src/Microservices/CatalogMicroservice/Catalog.API/DomainModels/Product.cs
+++ src/Microservices/CatalogMicroservice/Catalog.API/DomainModels/Product.cs
@@ -12,6 +12,8 @@ namespace Catalog.API.DomainModels
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
         public string Id { get; set; }
+
+        [BsonElement("Name")]
         public string Name { get; set; }
         public string Category { get; set; }
         public string Description { get; set; }
