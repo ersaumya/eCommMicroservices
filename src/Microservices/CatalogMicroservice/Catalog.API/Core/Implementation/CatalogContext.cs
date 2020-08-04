c2640227afa23ad0bf78ffdeb724df6fc6ab4226
ersaumya <ersaumyarout@gmail.com>
2020-08-04 19:55:16 +0530

    seed catalog data

--- src/Microservices/CatalogMicroservice/Catalog.API/Core/Implementation/CatalogContext.cs
+++ src/Microservices/CatalogMicroservice/Catalog.API/Core/Implementation/CatalogContext.cs
@@ -15,7 +15,8 @@ namespace Catalog.API.Core.Implementation
         {
             var client = new MongoClient(settings.ConnectionString);
             var database = client.GetDatabase(settings.DatabaseName);
-            Products = database.GetCollection<Product>(settings.CollectionName); 
+            Products = database.GetCollection<Product>(settings.CollectionName);
+            CatalogContextSeed.SeedData(Products);
         }
         public IMongoCollection<Product> Products { get; }
     }
---
+++ src/Microservices/CatalogMicroservice/Catalog.API/Core/Implementation/CatalogContextSeed.cs
@@ -0,0 +1,65 @@
+﻿using Catalog.API.DomainModels;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Core.Implementation
+{
+    public class CatalogContextSeed
+    {
+        public static void SeedData(IMongoCollection<Product> productCollection)
+        {
+            bool existProduct = productCollection.Find(p => true).Any();
+            if (!existProduct)
+            {
+                productCollection.InsertManyAsync(GetPreConfiguredProducts());
+            }
+        }
+
+        private static IEnumerable<Product> GetPreConfiguredProducts()
+        {
+            return new List<Product>()
+            {
+                new Product()
+                {
+                    Name="Iphone 11 pro",
+                    Summary="Best flagship phone of 2020",
+                    Description="Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+                    ImageFile="product1.jpeg",
+                    Price=117000,
+                    Category="Mobile"
+                },
+                new Product()
+                {
+                    Name="Samsung s20 ultra",
+                    Summary="Best flagship phone of 2020",
+                    Description="Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+                    ImageFile="product2.jpeg",
+                    Price=79000,
+                    Category="Mobile"
+                },
+                new Product()
+                {
+                    Name="Oneplus 8 pro",
+                    Summary="Best flagship phone of 2020",
+                    Description="Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+                    ImageFile="product3.jpeg",
+                    Price=54000,
+                    Category="Mobile"
+                },
+                new Product()
+                {
+                    Name="Mi 10",
+                    Summary="Best flagship phone of 2020",
+                    Description="Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+                    ImageFile="product4.jpeg",
+                    Price=49000,
+                    Category="Mobile"
+                }
+
+            };
+        }
+    }
+}
