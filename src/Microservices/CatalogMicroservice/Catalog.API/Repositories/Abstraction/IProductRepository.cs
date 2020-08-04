a6d686d4b2de56456abb5992b9c6055ee8b669b2
ersaumya <ersaumyarout@gmail.com>
2020-08-04 21:32:40 +0530

    product repository added

---
+++ src/Microservices/CatalogMicroservice/Catalog.API/Repositories/Abstraction/IProductRepository.cs
@@ -0,0 +1,20 @@
+﻿using Catalog.API.DomainModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Repositories.Abstraction
+{
+     
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetProducts();
+        Task<Product> GetProduct(string id);
+        Task<IEnumerable<Product>> GetProductByName(string name);
+        Task<IEnumerable<Product>> GetProductByCategory(string CategoryName);
+        Task Create(Product product);
+        Task<bool> Update(Product product);
+        Task<bool> Delete(Product product);
+    }
+}
---
+++ src/Microservices/CatalogMicroservice/Catalog.API/Repositories/Implementation/ProductRepository.cs
@@ -0,0 +1,47 @@
+﻿using Catalog.API.DomainModels;
+using Catalog.API.Repositories.Abstraction;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Repositories.Implementation
+{
+    public class ProductRepository : IProductRepository
+    {
+        public Task Create(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Delete(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Product> GetProduct(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Product>> GetProductByCategory(string CategoryName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Product>> GetProductByName(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Product>> GetProducts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Update(Product product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
