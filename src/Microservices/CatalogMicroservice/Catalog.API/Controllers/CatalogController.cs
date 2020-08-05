eb99a45c8008c6221b68c1ebb25f5e9b776d7f52
ersaumya <ersaumyarout@gmail.com>
2020-08-05 11:58:59 +0530

    get method added

---
+++ src/Microservices/CatalogMicroservice/Catalog.API/Controllers/CatalogController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Catalog.API.DomainModels;
+using Catalog.API.Repositories.Abstraction;
+using DnsClient.Internal;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MongoDB.Driver;
+
+namespace Catalog.API.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class CatalogController : ControllerBase
+    {
+        private readonly IProductRepository _repository;
+        private readonly ILogger<CatalogController> _logger;
+
+        public CatalogController(IProductRepository repository, ILogger<CatalogController> logger)
+        {
+            _repository = repository;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<Product>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+        {
+            var products = await _repository.GetProducts();
+            return Ok(products);
+
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(Product), (int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        public async Task<ActionResult<Product>> GetProductById(string id)
+        {
+            var product = await _repository.GetProduct(id);
+            if (product != null)
+            {
+                return Ok(product);
+            }
+            else
+            {
+                _logger.LogError($"Product with id:{id},not found.");
+                return NotFound();
+            }
+        }
+
+
+    }
+}
