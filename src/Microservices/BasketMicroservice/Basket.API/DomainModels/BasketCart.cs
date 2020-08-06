41cb984c4726a1805cdc53bb3b39e8fdce4a5eba
ersaumya <ersaumyarout@gmail.com>
2020-08-06 11:05:30 +0530

    basket models added

---
+++ src/Microservices/BasketMicroservice/Basket.API/DomainModels/BasketCart.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Basket.API.DomainModels
+{
+    public class BasketCart
+    {
+        public BasketCart()
+        {
+
+        }
+        public BasketCart(string userName)
+        {
+            UserName = userName;
+        }
+        public string UserName { get; set; }
+        public List<BasketCartItem> Items { get; set; } = new List<BasketCartItem>();
+
+        public decimal TotalPrice {
+            get
+            {
+                decimal totalPrice = 0;
+                foreach(var item in Items)
+                {
+                    totalPrice += item.Price * item.Quantity;
+                }
+                return totalPrice;
+            }
+        }
+    }
+}
