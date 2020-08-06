4ce9759ec8959b549cffa8782829e13b24a1500e
ersaumya <ersaumyarout@gmail.com>
2020-08-07 00:01:58 +0530

    get and update basket endpoint

---
+++ src/Microservices/BasketMicroservice/Basket.API/Controllers/BasketController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Basket.API.DomainModels;
+using Basket.API.Repositories.Abstraction;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Basket.API.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class BasketController : ControllerBase
+    {
+        private readonly IBasketRepository _repository;
+
+        public BasketController(IBasketRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(BasketCart),(int)HttpStatusCode.OK)]
+        public async Task<ActionResult<BasketCart>> GetBasket(string userName)
+        {
+            var basket = await _repository.GetBasket(userName);
+            return Ok(basket);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(BasketCart), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<BasketCart>> UpdateBasket([FromBody] BasketCart basketCart)
+        {
+            var basket = await _repository.UpdateBasket(basketCart);
+            return Ok(basket);
+        }
+    }
+}
