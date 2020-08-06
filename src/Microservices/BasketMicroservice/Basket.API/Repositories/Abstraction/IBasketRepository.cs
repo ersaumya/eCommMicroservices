bc76d83ece92f46711887e077001c3235d46bc43
ersaumya <ersaumyarout@gmail.com>
2020-08-06 16:13:01 +0530

    Basket repository added

---
+++ src/Microservices/BasketMicroservice/Basket.API/Repositories/Abstraction/IBasketRepository.cs
@@ -0,0 +1,15 @@
+﻿using Basket.API.DomainModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Basket.API.Repositories.Abstraction
+{
+    public interface IBasketRepository
+    {
+        Task<BasketCart> GetBasket(string userName);
+        Task<BasketCart> UpdateBasket(BasketCart basket);
+        Task<bool> DeleteBasket(string userName);
+    }
+}
