f1f8f5b66103b84be6df4d81bc0405eb9ee24984
ersaumya <ersaumyarout@gmail.com>
2020-08-08 13:08:45 +0530

    rabbitmq connection config

--- src/Common/EventBusRabbitMQ/IRabbitMQConnection.cs
+++ src/Common/EventBusRabbitMQ/IRabbitMQConnection.cs
@@ -1,10 +1,14 @@
-﻿using System;
+﻿using RabbitMQ.Client;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace EventBusRabbitMQ
 {
-    public interface IRabbitMQConnection
+    public interface IRabbitMQConnection: IDisposable
     {
+        bool IsConnected { get; }
+        bool TryConnect();
+        IModel CreateModel();
     }
 }
