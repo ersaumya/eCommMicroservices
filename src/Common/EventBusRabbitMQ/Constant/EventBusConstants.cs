376f0574e201bfe9b9eee8a50a40df076ffa8112
ersaumya <ersaumyarout@gmail.com>
2020-08-08 15:54:34 +0530

    basket checkout conatant added

---
+++ src/Common/EventBusRabbitMQ/Constant/EventBusConstants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EventBusRabbitMQ.Constant
+{
+    public static class EventBusConstants
+    {
+        public const string BasketCheckoutQueue = "basketCheckoutQueue";
+    }
+}
