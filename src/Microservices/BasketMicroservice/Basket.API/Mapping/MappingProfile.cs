b132d2dfaebd6edcd1bc7a7589f64626b9885a3c
ersaumya <ersaumyarout@gmail.com>
2020-08-08 17:00:43 +0530

    Automapper mapping profile

---
+++ src/Microservices/BasketMicroservice/Basket.API/Mapping/MappingProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Basket.API.DomainModels;
+using EventBusRabbitMQ.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Basket.API.Mapping
+{
+    public class MappingProfile:Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<BasketCheckout, BasketCheckoutEvent>().ReverseMap();
+        }
+    }
+}
