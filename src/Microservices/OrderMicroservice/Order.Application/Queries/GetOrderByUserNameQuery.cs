1a095c418f72316d445d32db33ee92b9252071dd
ersaumya <ersaumyarout@gmail.com>
2020-08-13 16:50:17 +0530

    queries and response added

---
+++ src/Microservices/OrderMicroservice/Order.Application/Queries/GetOrderByUserNameQuery.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using Order.Application.Responses;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Order.Application.Queries
+{
+    public class GetOrderByUserNameQuery:IRequest<IEnumerable<OrderResponse>>
+    {
+        public string UserName { get; set; }
+
+        public GetOrderByUserNameQuery(string userName)
+        {
+            UserName = userName;
+        }
+    }
+}
