14b579a0fe114c8956381a297bc5647fbad02282
ersaumya <ersaumyarout@gmail.com>
2020-08-13 17:05:37 +0530

    handerlers added

---
+++ src/Microservices/OrderMicroservice/Order.Application/Handlers/GetOrderByUserNameHandler.cs
@@ -0,0 +1,30 @@
+﻿using MediatR;
+using Order.Application.Queries;
+using Order.Application.Responses;
+using Order.Core.RepositoryContracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Order.Application.Handlers
+{
+    public class GetOrderByUserNameHandler:IRequestHandler<GetOrderByUserNameQuery,IEnumerable<OrderResponse>>
+    {
+        private readonly IOrderRepository _orderRepository;
+
+        public GetOrderByUserNameHandler(IOrderRepository orderRepository)
+        {
+            _orderRepository = orderRepository;
+        }
+
+        public async Task<IEnumerable<OrderResponse>> Handle(GetOrderByUserNameQuery request, CancellationToken cancellationToken)
+        {
+            var orderList = await _orderRepository.GetOrdersByUserName(request.UserName);
+
+            var orderResponseList = OrderMapper.Mapper.Map<IEnumerable<OrderResponse>>(orderList);
+            return orderResponseList;
+        }
+    }
+}
