a419b1588478939c16ea092296f7ead9b32e4d4f
ersaumya <ersaumyarout@gmail.com>
2020-08-13 17:11:54 +0530

    mapping added

---
+++ src/Microservices/OrderMicroservice/Order.Application/Mapper/OrderMappingProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using Order.Application.Responses;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Order.Application.Mapper
+{
+    public class OrderMappingProfile : Profile
+    {
+        public OrderMappingProfile()
+        {
+            CreateMap<Core.Entities.Order, CheckoutOrderCommand>().ReverseMap();
+            CreateMap<Core.Entities.Order, OrderResponse>().ReverseMap();
+        }
+    }
+}
