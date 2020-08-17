e76ae2d81b53ff76048b1e540d6f76f5488b05af
ersaumya <ersaumyarout@gmail.com>
2020-08-17 23:09:42 +0530

    order controller endpoint added

--- src/Microservices/OrderMicroservice/Order.API/Controllers/OrderController.cs
+++ src/Microservices/OrderMicroservice/Order.API/Controllers/OrderController.cs
@@ -1,9 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
+using MediatR;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Order.Application.Commands;
+using Order.Application.Queries;
+using Order.Application.Responses;
 
 namespace Order.API.Controllers
 {
@@ -11,5 +16,30 @@ namespace Order.API.Controllers
     [ApiController]
     public class OrderController : ControllerBase
     {
+        private readonly IMediator _mediator;
+
+        public OrderController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<OrderResponse>),(int)HttpStatusCode.OK)]
+        public async Task<ActionResult<IEnumerable<OrderResponse>>> RetriveOrdersByUserName(string userName)
+        {
+            var query = new GetOrderByUserNameQuery(userName);
+            var orders = await _mediator.Send(query);
+            return Ok(orders);
+        }
+
+        //testing purpose
+        [HttpPost]
+        [ProducesResponseType(typeof(OrderResponse), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<OrderResponse>> CheckoutOrders([FromBody] CheckoutOrderCommand command)
+        {
+            
+            var result = await _mediator.Send(command);
+            return Ok(result);
+        }
     }
 }
