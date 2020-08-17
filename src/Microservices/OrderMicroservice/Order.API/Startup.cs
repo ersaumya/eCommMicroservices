8a85c090ed89df43bdd0fe7da5625dbfb2247361
ersaumya <ersaumyarout@gmail.com>
2020-08-17 23:22:07 +0530

    dependancy added

--- src/Microservices/OrderMicroservice/Order.API/Startup.cs
+++ src/Microservices/OrderMicroservice/Order.API/Startup.cs
@@ -1,7 +1,10 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
+using AutoMapper;
+using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -10,7 +13,12 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Order.Application.Handlers;
+using Order.Core.RepositoryContracts;
+using Order.Core.RepositoryContracts.Base;
 using Order.Infrastructure.Data;
+using Order.Infrastructure.RepositoryImplementations;
+using Order.Infrastructure.RepositoryImplementations.Base;
 
 namespace Order.API
 {
@@ -30,6 +38,11 @@ namespace Order.API
 
             services.AddDbContext<OrderContext>(c =>
                 c.UseSqlServer(Configuration.GetConnectionString("OrderConnection")), ServiceLifetime.Singleton);
+            services.AddAutoMapper(typeof(Startup));
+            services.AddMediatR(typeof(CheckoutOrderHandler).GetTypeInfo().Assembly);
+            services.AddTransient<IOrderRepository, OrderRepository>();
+            services.AddScoped(typeof(IRepository<>),typeof(Repository<>));
+            services.AddScoped(typeof(IOrderRepository),typeof(OrderRepository));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
