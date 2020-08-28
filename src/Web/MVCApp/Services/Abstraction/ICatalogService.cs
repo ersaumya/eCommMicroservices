69007abcf5158fd7053fb58ae440c92e6ba14596
ersaumya <ersaumyarout@gmail.com>
2020-08-28 12:05:39 +0530

    catalog services and model added

---
+++ src/Web/MVCApp/Services/Abstraction/ICatalogService.cs
@@ -0,0 +1,16 @@
+﻿using MVCApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCApp.Services.Abstraction
+{
+    public interface ICatalogService
+    {
+        Task<IEnumerable<Catalog>> GetCatalogs();
+        Task<IEnumerable<Catalog>> GetCatalogByCategory(string category);
+        Task<Catalog> GetCatalog(string id);
+        Task<Catalog> CreateCatalog(Catalog model);
+    }
+}
