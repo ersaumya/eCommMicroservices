517f8939ef0bfc4e81c835aa84ff152f3a7dcddb
ersaumya <ersaumyarout@gmail.com>
2020-11-17 20:41:41 +0530

    delete web app

---
+++ src/Web/MVCApp/Controllers/CatalogController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MVCApp.Services.Abstraction;
+
+namespace MVCApp.Controllers
+{
+    public class CatalogController : Controller
+    {
+        private readonly ICatalogService _catalogService;
+        private readonly IBasketService _basketService;
+
+        public CatalogController(ICatalogService catalogService, IBasketService basketService)
+        {
+            _catalogService = catalogService;
+            _basketService = basketService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
